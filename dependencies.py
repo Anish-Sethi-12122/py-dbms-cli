8bffeb70656ef4c1b1306413ef4db759d6244588
Anish Sethi <143344217+Anish-Sethi-12122@users.noreply.github.com>
2025-12-27 17:42:26 +0530

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,19 +1,10 @@
 #Dependencies imports
 
-import mysql.connector as mysql
-import sqlparse
-import pwinput
-import time
-import sys
-from rich.console import Console
+import mysql.connector as mysql, sqlparse, pwinput, time, sys, pyfiglet, re, os, json, copy
+from rich.console import Console, Group
 from rich.text import Text
 from rich.panel import Panel
 from rich import box
-import pyfiglet
 from rich.table import Table
 from rich.align import Align
 from rich.rule import Rule
-import re
-import os
-import json
-import copy
