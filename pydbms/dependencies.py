785812d136e50d8e21c664a6196b0496f11acb0c
Anish Sethi <143344217+Anish-Sethi-12122@users.noreply.github.com>
2026-01-17 01:19:11 +0530

    Update dependencies.py

--- pydbms/dependencies.py
+++ pydbms/dependencies.py
@@ -1,6 +1,6 @@
-# pydbms/pydbms/dependencies.py
+# pydbms/pydbms/main/dependencies.py
 
-import mysql.connector as mysql, sqlparse, pwinput, time, sys, pyfiglet, re, os, json, copy, datetime, csv
+import mysql.connector as mysql
 from rich.console import Console, Group
 from rich.text import Text
 from rich.panel import Panel
@@ -9,4 +9,20 @@ from rich.table import Table
 from rich.align import Align
 from rich.rule import Rule
 from dataclasses import dataclass
-from typing import List, Any
+from typing import List, Any, Optional, Protocol
+from crypto_functions import hash_argon2
+import shlex
+import sqlparse
+import pwinput
+import time
+import sys
+import pyfiglet
+import re
+import os
+import json
+import copy
+from datetime import datetime
+import csv
+import decimal
+import base64
+from importlib.metadata import version as pydbms_version
