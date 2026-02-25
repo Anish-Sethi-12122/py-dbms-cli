131f5e67fc4e71ce59adf8666363d2e822f0a9da
Anish Sethi <143344217+Anish-Sethi-12122@users.noreply.github.com>
2026-02-25 23:28:19 +0530

    Create runtime.py

---
+++ main/runtime.py
@@ -0,0 +1,51 @@
+# pydbms/pydbms/main/runtime.py
+
+import time
+from datetime import datetime
+from .dependencies import pydbms_version
+import copy
+from .config import load_config, DEFAULT_CONFIG
+from rich.console import Console
+
+console=Console()
+config = load_config()
+ver = pydbms_version(distribution_name="py-dbms-cli")
+
+def Print(message: str, color_key: str ="WHITE", style: str = "", slow_type: bool = True) -> None:
+    COLOR_MAP = {
+        "CYAN": "bright_cyan",
+        "YELLOW": "bright_yellow",
+        "RED": "bright_red",
+        "GREEN": "bright_green",
+        "WHITE": "white",
+        "MAGENTA": "bright_magenta"
+    }
+    color = COLOR_MAP.get(color_key, "white")
+    delay = 0.02069 if slow_type else 0
+    for char in message:
+        if char == "\n":
+            console.print()
+            continue
+        console.print(char, style=f"{style} {color}", end="")
+        time.sleep(delay)
+        
+def current_datetime() -> str:
+    current_datetime = datetime.datetime.now()
+
+    Year = current_datetime.year
+    Month = current_datetime.month
+    Day = current_datetime.day
+    Hour = current_datetime.hour
+    Minute = current_datetime.minute
+    Second = current_datetime.second
+    
+    return f"{Year}-{Month}-{Day}_{Hour}-{Minute}-{Second}"
+
+def load_config_safe() -> dict:
+    try:
+        return load_config()
+    except Exception as e:
+        Print(f"\nError loading config: {e}\n", "RED")
+        return copy.deepcopy(DEFAULT_CONFIG)
+
+config = load_config_safe()
