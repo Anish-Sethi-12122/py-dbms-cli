f646b9c65eba078605e3d267ac9db75848f48b58
Anish Sethi <143344217+Anish-Sethi-12122@users.noreply.github.com>
2026-02-25 23:23:08 +0530

    Update mysql.py

--- db/mysql.py
+++ db/mysql.py
@@ -1,9 +1,12 @@
 # pydbms/db/mysql.py
 
-from ..main.dependencies import pwinput, mysql, hash_argon2
+from ..main.dependencies import mysql
+import pwinput
+from crypto_functions import hash_argon2
 from .db_base import DBConnector
+from .db_exceptions import DatabaseError, ConnectionError, QueryError
 from ..main.runtime import Print
-from ..main.profile import MySQLProfile
+from ..main.profile import pydbmsProfile
 from ..main import profile
 
 class MySQLConnector(DBConnector):
@@ -21,6 +24,10 @@ class MySQLConnector(DBConnector):
         self.user = input().strip() or user
 
         self.password = pwinput.pwinput("pydbms> Enter password: ", "*")
+        
+    @property
+    def exception_class(self):
+        return mysql.Error
 
     def connect(self) -> tuple[object, object] | tuple[None, None]:
         try:
@@ -33,16 +40,13 @@ class MySQLConnector(DBConnector):
 
             Print("✅ Login successful.", "GREEN")
             
-            profile.PROFILE = MySQLProfile(
+            profile.PROFILE = pydbmsProfile.MySQL(
                 host=self.host,
                 user=self.user,
-                password_hash=hash_argon2(self.password)
+                password_hash=hash_argon2(self.password) # For session persistence (in future versions)
             )
             
             return self.connection, self.cursor
 
-        except mysql.Error:
-            Print(f"❌ Incorrect Credentials entered. Try again\n\n", "RED", "bold")
-            return None, None
-        
-        
+        except mysql.Error as e:
+            raise ConnectionError(f"Connection failed: {e}") from e
